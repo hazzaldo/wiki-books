f54b2dd1baf311e9d85219873cf25f9a8b0b774d
hazzaldo <hazzaldo@hotmail.com>
2020-06-01 17:44:50 +0100

    create App component as jsx file and nest BookDirectory component in it

---
+++ client/src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// components
+import BookDirectory from './BookDirectory';
+
+function App() {
+  return (
+    <div id="main">
+      <h1>Wiki Books</h1>
+      <BookDirectory />
+    </div>
+  );
+}
+
+export default App;
