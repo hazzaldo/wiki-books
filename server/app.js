561cfcc63643c5bd5e10efa51f87570bbddefc03
hazzaldo <hazzaldo@hotmail.com>
2020-06-01 17:43:41 +0100

    change backend server to listen to port 4000 as port 3000 used by React app by default

--- server/app.js
+++ server/app.js
@@ -28,8 +28,8 @@ app.use('/graphql', graphqlHTTP({
     graphiql: true
 }));
 
-const port = 3000;
+const port = 4000;
 
 app.listen(port, () => {
     console.log(`Server started listening on port ${port}`);
-})
+});
