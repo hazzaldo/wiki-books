69125872b3457ec774474197a52298263e38335d
hazzaldo <hazzaldo@hotmail.com>
2020-06-01 21:32:35 +0100

    add GraphQL query to retrieve All Books and render in BookDirectory component

--- client/src/components/BookDirectory.jsx
+++ client/src/components/BookDirectory.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from "apollo-boost";
+
+const GET_ALL_BOOKS_QUERY = gql`
+  {
+    books{
+      id
+      name
+      genre
+    }
+  }
+`;
 
 function BookDirectory() {
+  const { loading, error, data } = useQuery(GET_ALL_BOOKS_QUERY);
+  function displayBooks () {
+    if (loading) return ('Loading books...');
+    if (error) return (`Error: ${error.message}`);
+    return data.books.map(book => {
+      return (
+      <li key={book.id}>{book.name}</li>
+    )});
+  }
+  
   return (
     <div>
         <ul id="book-directory">
-            <li>Book name</li>
+            {displayBooks()}
         </ul>
     </div>
   );
