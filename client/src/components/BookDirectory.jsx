096a41d50c614be073a7793cceab89c61538a9c2
hazzaldo <hazzaldo@hotmail.com>
2020-06-01 17:42:47 +0100

    create BookDirectory.jsx component

---
+++ client/src/components/BookDirectory.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function BookDirectory() {
+  return (
+    <div>
+        <ul id="book-directory">
+            <li>Book name</li>
+        </ul>
+    </div>
+  );
+}
+
+export default BookDirectory;
