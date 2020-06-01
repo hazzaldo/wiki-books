9206cce8caf5c6b9c790dc1d9b6bcb16abfbf4e6
hazzaldo <hazzaldo@hotmail.com>
2020-06-01 21:31:48 +0100

    correct Apollo setup (not supposed to be in App.jsx component but in index.js instead)

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,23 +1,14 @@
 import React from 'react';
-import ApolloClient from 'apollo-boost';
-import ApolloProvider from '@apollo/react-hooks';
 
 // components
 import BookDirectory from './BookDirectory';
 
-// Apollo client setup 
-const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql'
-});
-
 function App() {
   return(
-    <ApolloProvider client={client}>
-      <div id="main">
-        <h1>Wiki Books</h1>
-        <BookDirectory />
-      </div>
-    </ApolloProvider>
+    <div id="main">
+      <h1>Wiki Books</h1>
+      <BookDirectory />
+    </div>
   );
 }
 
