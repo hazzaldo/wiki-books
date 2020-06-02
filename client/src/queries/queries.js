39cad782a889ea54fbb074d1e7ed6498872a3c66
hazzaldo <hazzaldo@hotmail.com>
2020-06-02 23:47:57 +0100

    add GET_BOOK_QUERY (graphQL query) to fetch a book based on its id

--- client/src/queries/queries.js
+++ client/src/queries/queries.js
@@ -21,12 +21,30 @@ const GET_ALL_AUTHORS_QUERY = gql`
 
 // you can name your mutation if you want (it's optional), like: mutation AddBook($name: String!, $Genre: String!, $authorId: ID)
 const ADD_BOOK_MUTATION = gql`
-  mutation($name: String!, $genre: String!, $authorId: ID!) {
-    addBook(name: $name, genre: $genre, authorId: $authorId) {
+  mutation($name: String!, $genre: String!, $authorId: ID!){
+    addBook(name: $name, genre: $genre, authorId: $authorId){
       name
       id
     }
   }
 `
+const GET_BOOK_QUERY = gql`
+  query($id: ID){
+    book(id: $id){
+      id
+      name
+      genre
+      author{
+        id
+        name
+        age
+        books{
+          id
+          name
+        }
+      }
+    }
+  }
+`;
 
-export { GET_ALL_BOOKS_QUERY, GET_ALL_AUTHORS_QUERY, ADD_BOOK_MUTATION };
+export { GET_ALL_BOOKS_QUERY, GET_ALL_AUTHORS_QUERY, ADD_BOOK_MUTATION, GET_BOOK_QUERY };
