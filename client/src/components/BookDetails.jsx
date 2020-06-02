6484b5a7b876c94355bb1de9c76c9d645565f61d
hazzaldo <hazzaldo@hotmail.com>
2020-06-02 23:49:39 +0100

    add BookDetails component along with supporting code from BookDirectory component to pass the bookId prop

---
+++ client/src/components/BookDetails.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { GET_BOOK_QUERY } from '../queries/queries';
+
+function BookDetails(props) {
+
+  function isEmpty(value){
+    return  value === undefined ||
+            value === null ||
+            (typeof value === "object" && Object.keys(value).length === 0) ||
+            (typeof value === "string" && value.trim().length === 0)
+  }
+  
+  const { loading, error, data } = useQuery(GET_BOOK_QUERY, {
+    variables: { id: props.bookId },
+    skip: isEmpty(props.bookId)
+  });
+
+  function displayBookDetails() {
+    if (isEmpty(data)) {
+      return <div>No book selected...</div>
+    } else {
+      if (loading) {
+        return <div>Loading book details...</div>
+      }
+      if (error) {
+        return <div>Error: {error.message}</div>
+      }
+      const { book } = data;
+      if (book) {
+        return (
+          <div>
+            <h2>{ book.name } </h2>
+            <p>{ book.genre }</p>
+            <p>{ book.author.name }</p>
+            <p>{ book.author.age }</p>
+            <p>Other books written by this author: </p>
+            <ul className="other-books">
+              {book.author.books.map(book => {
+                return <li key={book.id}>Book name: {book.name}. Genre: {book.genre}</li>
+              })}
+            </ul>
+          </div>
+        )
+      } else {
+        return <div>No book selected...</div>
+      }
+    }
+  }
+  
+  return (
+    <div id="book-details">
+      <p>Book details</p>
+        {displayBookDetails()}
+    </div>
+  );
+}
+
+export default BookDetails;
--- client/src/components/BookDirectory.jsx
+++ client/src/components/BookDirectory.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import { GET_ALL_BOOKS_QUERY } from '../queries/queries';
+import BookDetails from './BookDetails'
 
 function BookDirectory() {
+  const [ selectedBookId, setSelectedBookId ] = useState("");
   const { loading, error, data } = useQuery(GET_ALL_BOOKS_QUERY);
-  function displayBooks () {
+
+  function selectBook(bookId) {
+    setSelectedBookId(bookId);
+  }
+
+  function displayBooks() {
     if (loading) return ('Loading books...');
     if (error) return (`Error: ${error.message}`);
     return data.books.map(book => {
       return (
-      <li key={book.id}>{book.name}</li>
+      // You can alternatively pass the Hook State Setter directly to onClick and pass the book.id inside of it like so: onClick={(e) => {setSelectedBookId(book.id)} 
+      <li onClick={selectBook.bind(null, book.id)} key={book.id}>{book.name}</li>
     )});
   }
   
@@ -18,6 +26,7 @@ function BookDirectory() {
         <ul id="book-directory">
             {displayBooks()}
         </ul>
+        <BookDetails bookId={selectedBookId} />
     </div>
   );
 }
